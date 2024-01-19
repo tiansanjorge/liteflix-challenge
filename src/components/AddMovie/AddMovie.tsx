abe1816651a15a96656f72e07568f58e1a8fb211
Sebastian Sanjorge <sanjorgesebastian@gmail.com>
2024-01-19 02:03:00 -0300

    improving modal behavior

--- src/components/AddMovie/AddMovie.tsx
+++ src/components/AddMovie/AddMovie.tsx
@@ -1,4 +1,6 @@
 import React, { useCallback, useState, useEffect } from "react";
+import { Modal } from "react-bootstrap";
+import MobileMenuHeader from "../MobileMenuHeader/MobileMenuHeader";
 import AddingMovie from "../Modals/AddingMovie/AddingMovie";
 import UploadingMovie from "../Modals/UploadingMovie/UploadingMovie";
 import ErrorUploading from "../Modals/ErrorUploading/ErrorUploading";
@@ -29,6 +31,19 @@ const AddMovie: React.FC<AddMovieProps> = ({ show, onClose }) => {
   const [movieData, setMovieData] = useState<MovieData | null>(null);
   const [fileUploaded, setFileUploaded] = useState(false);
   const [currentModal, setCurrentModal] = useState<CurrentModal>(null);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   useEffect(() => {
     if (show === true) {
@@ -49,6 +64,7 @@ const AddMovie: React.FC<AddMovieProps> = ({ show, onClose }) => {
         localStorage.setItem("movieData", JSON.stringify(fileData));
         setMovieData(fileData);
         setCurrentModal("UploadingMovie");
+        console.log(currentModal);
         setFileUploaded(true);
         setTimeout(() => {
           setCurrentModal("ErrorUploading");
@@ -101,24 +117,79 @@ const AddMovie: React.FC<AddMovieProps> = ({ show, onClose }) => {
 
   return (
     <>
-      {currentModal === "AddingMovie" && (
-        <>  
-        <AddingMovie
-          show={show}
-          onReset={handleReset}
-          onDrop={handleDrop}
-          onDragOver={handleDragOver}
-          handleFileChange={handleFileChange}
-        />
-        </>
-      )}
-      {currentModal === "UploadingMovie" && (
-        <UploadingMovie
-          fileUploaded={fileUploaded}
-          movieData={movieData}
-          onReset={handleReset}
-        />
-      )}
+      <Modal show={show} onHide={handleReset} centered className="add-movie">
+        {windowWidth < 900 && <MobileMenuHeader onClose={handleReset} />}
+        <div className="add-movie__content">
+          <h3 className="add-movie__title">
+            AGREGAR PELÍCULA
+          </h3>
+
+          {currentModal === "AddingMovie" && (
+            <div
+              className="add-movie__container"
+              onDrop={handleDrop}
+              onDragOver={handleDragOver}
+            >
+              <input
+                type="file"
+                accept="image/*"
+                style={{ display: "none" }}
+                onChange={(event) => {
+                  const file = event.target.files?.[0];
+                  handleFileChange(file);
+                }}
+                id="upload-input"
+              />
+              <label
+                className="add-movie__container-label"
+                htmlFor="upload-input"
+              >
+                <img
+                  className="add-movie__container-clip"
+                  src="img/clip.png"
+                  alt="clip"
+                />
+                {windowWidth < 900
+                  ? "AGREGA UN ARCHIVO"
+                  : "AGREGÁ UN ARCHIVO O ARRASTRALO Y SOLTALO AQUÍ"}
+              </label>
+            </div>
+          )}
+          {currentModal === "UploadingMovie" && (
+            <div className="add-movie__loading">
+              <p className="add-movie__loading-label">
+                CARGANDO <b>40%</b>
+              </p>
+              <progress
+                className="add-movie__loading-progress"
+                id="file"
+                max="100"
+                value="40"
+              >
+                40%
+              </progress>
+              <p className="add-movie__loading-cancel" onClick={handleReset}>
+                CANCELAR
+              </p>
+            </div>
+          )}
+          <div className="add-movie__divider">
+            <p className="add-movie__divider-title">TITULO</p>
+          </div>
+            <div className="add-movie__footer">
+            <button className="add-movie__upload-button" disabled>
+              SUBIR PELÍCULA
+            </button>
+            {windowWidth < 900 && (
+              <button className="add-movie__exit-button" onClick={handleReset}>
+                SALIR
+              </button>
+            )}
+            </div>
+        </div>
+      </Modal>
+
+      {/* 
       {currentModal === "ErrorUploading" && (
         <ErrorUploading
           fileUploaded={fileUploaded}
@@ -143,7 +214,7 @@ const AddMovie: React.FC<AddMovieProps> = ({ show, onClose }) => {
           onReset={handleReset}
           onGoToHome={handleGoToHome}
         />
-      )}
+      )} */}
     </>
   );
 };
